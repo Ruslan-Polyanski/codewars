e9a4d525f42eef04e599e57797b086b8bcdf13b6
Ruslan-Palyanski <ruslapolyanski@yandex.ru>
2023-08-06 18:04:42 +0300

    add new sulutions

--- Two Sum/index.js
+++ Two Sum/index.js
@@ -1,12 +1,34 @@
 
+// const twoSum = function(nums, target) {
+//   const numsArr = [...nums];
+//   const numsArrLength = numsArr.length;
+//   let result = null;
+//   for(let i = 0; i < numsArrLength; ++i){
+//     for(let j = i + 1; j < numsArrLength; ++j){
+//       numsArr[i] + numsArr[j] === target ? result = [i, j] : null;
+//     }
+//   }
+//   return result;
+// };
+
+
+
 const twoSum = function(nums, target) {
   const numsArr = [...nums];
   const numsArrLength = numsArr.length;
-  let result = null;
+  const store = {};
   for(let i = 0; i < numsArrLength; ++i){
-    for(let j = i + 1; j < numsArrLength; ++j){
-      numsArr[i] + numsArr[j] === target ? result = [i, j] : null;
+    const result = target - numsArr[i];
+    if(result in store){
+      return [store[result], i];
+    } else {
+      store[numsArr[i]] = i;
     }
   }
-  return result;
 };
+
+
+
+twoSum([2,7,11,15], 9) // [0, 1]
+twoSum([3,2,4], 6)  // [1,2]
+twoSum([3,3], 6) // [0,1]
